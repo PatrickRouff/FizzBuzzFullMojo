f894aff54c1ea72b09d882b212f5b152ce2102ef
Demetri Case <39623681+demetrioscase@users.noreply.github.com>
2018-06-22 15:54:55 -0500

    added squirrel route

--- my_app/lib/MyApp.pm
+++ my_app/lib/MyApp.pm
@@ -27,7 +27,8 @@ sub startup {
   $r->get('/guide')->to('menu#guide');
   $r->websocket('/hhhr')->to('hhh#hhhr');
   $r->get('/modes')->to('multi#modes');
-  $r->get('/hhh')->to('multi#hhh')
+  $r->get('/hhh')->to('multi#hhh');
+  $r->get('/squirrel')->to('multi#squirrel');
 }
 
 1;
