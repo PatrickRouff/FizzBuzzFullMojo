e097aa1076694822529d24a4fc9b49dd549598d7
PatrickRouff <patrickrouff@gmail.com>
2018-06-25 11:06:17 -0500

    Got Single Up and running, multi is broken atm

--- my_app/lib/MyApp.pm
+++ my_app/lib/MyApp.pm
@@ -26,8 +26,8 @@ sub startup {
   $r->get('/hhh')->to('multi#hhh');
   $r->get('/guide')->to('menu#guide');
   $r->websocket('/hhhr')->to('hhh#hhhr');
+  $r->websocket('/hhhs')->to('hhh#hhhs');
   $r->get('/modes')->to('multi#modes');
-  $r->get('/hhh')->to('multi#hhh')
 }
 
 1;
